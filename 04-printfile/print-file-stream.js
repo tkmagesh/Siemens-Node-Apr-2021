5ab16a91efea7e3641291ab4bb963340061ac1f2
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-07 16:30:03 +0530

    reading file using streams

---
+++ 04-printfile/print-file-stream.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream('./sample.txt', { encoding : 'utf8', highWaterMark : 32 * 1024});
+
+//events - open, data, end, close, error
+
+var readCount = 0;
+
+stream.on('open', function(){
+    console.log('file is opened for reading');
+});
+
+stream.on('data', function(chunk){
+    ++readCount;
+    console.log(chunk);
+});
+
+stream.on('end', function(){
+    console.log('Thats all folks!');
+    console.log('read completed with ' + readCount + ' chunks!');
+});
+
+stream.on('close', function(){
+    console.log('file is closed');
+});
+
+stream.on('error', function(err){
+    console.log('something went wrong!');
+    console.log(err);
+})
