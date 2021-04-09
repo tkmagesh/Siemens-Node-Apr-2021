6cdf39e921c0e848323efd75f95499d28a5f9a2a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-09 11:05:04 +0530

    using npm

--- 08-web-calculator-server-refactored/logger.js
+++ 08-web-calculator-server-refactored/logger.js
@@ -1,10 +1,12 @@
+var chalk = require('chalk');
+
 function logger(req, res, next){
     var startTime = new Date();
-    var logMessage = req.method + '\t' + req.urlObj.pathname;
+    var logMessage = chalk.red(req.method) + '\t' + chalk.yellow(req.urlObj.pathname);
     res.on('finish', function(){
         var endTime = new Date(),
             elapsed = endTime - startTime;
-        logMessage += '\t' + res.statusCode + '\t' + elapsed + 'ms';
+        logMessage += '\t' + chalk.blue(res.statusCode) + '\t' + chalk.bgCyan(elapsed) + 'ms';
         console.log(logMessage);
     });
     next();
